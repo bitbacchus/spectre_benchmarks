ac981a57f6d398b56e4de0e008f1a3673806c337
github-action-benchmark <github@users.noreply.github.com>
2021-01-25 21:34:25 +0000

    add Catch2 Benchmark (catch2) benchmark result for 7fece0e9aadc1d40562dd8b8e5aef7d7c5fd6aec

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1611610053204,
+  "lastUpdate": 1611610465774,
   "repoUrl": "https://github.com/bitbacchus/spectre_benchmarks",
   "entries": {
     "Catch2 Benchmark": [
@@ -36,6 +36,58 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 20.6141,
             "range": "± 1.23834",
+            "unit": "us",
+            "extra": "100 samples\n2 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sebastian@hanss.info",
+            "name": "Sebastian Hanß",
+            "username": "bitbacchus"
+          },
+          "committer": {
+            "email": "sebastian@hanss.info",
+            "name": "Sebastian Hanß",
+            "username": "bitbacchus"
+          },
+          "distinct": true,
+          "id": "7fece0e9aadc1d40562dd8b8e5aef7d7c5fd6aec",
+          "message": "tests",
+          "timestamp": "2021-01-25T22:32:33+01:00",
+          "tree_id": "38dbafa1564e816e0238a6bc7542ee3141e44b5c",
+          "url": "https://github.com/bitbacchus/spectre_benchmarks/commit/7fece0e9aadc1d40562dd8b8e5aef7d7c5fd6aec"
+        },
+        "date": 1611610464936,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 186.292,
+            "range": "± 2.63819",
+            "unit": "us",
+            "extra": "100 samples\n1 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 18.3687,
+            "range": "± 387.007",
+            "unit": "us",
+            "extra": "100 samples\n2 iterations"
+          },
+          {
+            "name": "Fibonacci 10a",
+            "value": 146.017,
+            "range": "± 0.33605",
+            "unit": "ns",
+            "extra": "100 samples\n218 iterations"
+          },
+          {
+            "name": "Fibonacci 20b",
+            "value": 18.3552,
+            "range": "± 354.596",
             "unit": "us",
             "extra": "100 samples\n2 iterations"
           }
