200e96a3673bc0bf71eef23a689217a80a602d54
github-action-benchmark <github@users.noreply.github.com>
2021-01-25 21:43:57 +0000

    add Catch2 Benchmark (catch2) benchmark result for 028660fe4a8b72c96e0d9c38fdf2d8cbf4474b7d

---
+++ dev/bench/data.js
@@ -0,0 +1,60 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1611611037181,
+  "repoUrl": "https://github.com/bitbacchus/spectre_benchmarks",
+  "entries": {
+    "Catch2 Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "sebastian@hanss.info",
+            "name": "Sebastian Hanß",
+            "username": "bitbacchus"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "028660fe4a8b72c96e0d9c38fdf2d8cbf4474b7d",
+          "message": "Update README.md",
+          "timestamp": "2021-01-25T22:43:05+01:00",
+          "tree_id": "f7f1adabda8a45cf23a554602a35dd3a4e429f16",
+          "url": "https://github.com/bitbacchus/spectre_benchmarks/commit/028660fe4a8b72c96e0d9c38fdf2d8cbf4474b7d"
+        },
+        "date": 1611611036389,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 175.642,
+            "range": "± 29.6505",
+            "unit": "ns",
+            "extra": "100 samples\n210 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 20.0065,
+            "range": "± 2.31328",
+            "unit": "us",
+            "extra": "100 samples\n2 iterations"
+          },
+          {
+            "name": "Fibonacci 10a",
+            "value": 175.038,
+            "range": "± 18.3722",
+            "unit": "ns",
+            "extra": "100 samples\n205 iterations"
+          },
+          {
+            "name": "Fibonacci 20b",
+            "value": 21.809,
+            "range": "± 1.59882",
+            "unit": "us",
+            "extra": "100 samples\n2 iterations"
+          }
+        ]
+      }
+    ]
+  }
+}
