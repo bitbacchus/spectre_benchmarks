8202ba1b19ee8e6c697bd4cdf1f86fbf4ef9a8e2
github-action-benchmark <github@users.noreply.github.com>
2021-02-08 13:07:33 +0000

    add Spectre Benchmark (catch2) benchmark result for 5acfdb6b4e18595de7a59c587317a3ae236db7ad

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1612785233363,
+  "lastUpdate": 1612789653510,
   "repoUrl": "https://github.com/bitbacchus/spectre_benchmarks",
   "entries": {
     "Spectre Benchmark": [
@@ -43,6 +43,58 @@ window.BENCHMARK_DATA = {
             "name": "opt",
             "value": 1.69278,
             "range": "± 564.726",
+            "unit": "ms",
+            "extra": "100 samples\n1 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sebastian@hanss.info",
+            "name": "Sebastian Hanß",
+            "username": "bitbacchus"
+          },
+          "committer": {
+            "email": "sebastian@hanss.info",
+            "name": "Sebastian Hanß",
+            "username": "bitbacchus"
+          },
+          "distinct": true,
+          "id": "5acfdb6b4e18595de7a59c587317a3ae236db7ad",
+          "message": "14",
+          "timestamp": "2021-02-08T14:04:20+01:00",
+          "tree_id": "09e61bb9dab7d4a5483d05fb2b261ad480421de4",
+          "url": "https://github.com/bitbacchus/spectre_benchmarks/commit/5acfdb6b4e18595de7a59c587317a3ae236db7ad"
+        },
+        "date": 1612789652909,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "MinConf",
+            "value": 2.06823,
+            "range": "± 709.952",
+            "unit": "ms",
+            "extra": "100 samples\n1 iterations"
+          },
+          {
+            "name": "Fibonacci 10",
+            "value": 213.831,
+            "range": "± 6.45727",
+            "unit": "ns",
+            "extra": "100 samples\n181 iterations"
+          },
+          {
+            "name": "opt",
+            "value": 2.04566,
+            "range": "± 708.84",
+            "unit": "ms",
+            "extra": "100 samples\n1 iterations"
+          },
+          {
+            "name": "aMinConf",
+            "value": 2.06667,
+            "range": "± 891.192",
             "unit": "ms",
             "extra": "100 samples\n1 iterations"
           }
