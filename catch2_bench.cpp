b75d699896f69de0d4d6e793eea48b28f7cbecad
Sebastian Hanß <sebastian@hanss.info>
2021-02-09 10:04:26 +0100

    42

--- catch2_bench.cpp
+++ catch2_bench.cpp
@@ -5,11 +5,6 @@
 TEST_CASE("Fibonacci") {
 
   // now let's benchmark:
-  BENCHMARK("Fibonacci 10") { return fib(8); };
+  BENCHMARK("test1234567890test1234567890test1234567890") { return fib(10); };
 
-  BENCHMARK("Fibonacci 20") { return fib(20); };
-
-  BENCHMARK("Fibonacci 10a") { return fib(10); };
-
-  BENCHMARK("Fibonacci 20b") { return fib(20); };
 }
