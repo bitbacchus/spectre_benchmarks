c8eb8d094db4b246c729a87cd6af6171eb537754
github-action-benchmark <github@users.noreply.github.com>
2021-02-09 09:12:24 +0000

    add Catch2 Benchmark (catch2) benchmark result for 1ca7c888407c674405382dac90ef166fa5a35892

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1612861944360,
+  "repoUrl": "https://github.com/bitbacchus/spectre_benchmarks",
+  "entries": {
+    "Catch2 Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "sebastian@hanss.info",
+            "name": "Sebastian Hanß",
+            "username": "bitbacchus"
+          },
+          "committer": {
+            "email": "sebastian@hanss.info",
+            "name": "Sebastian Hanß",
+            "username": "bitbacchus"
+          },
+          "distinct": true,
+          "id": "1ca7c888407c674405382dac90ef166fa5a35892",
+          "message": "28",
+          "timestamp": "2021-02-09T10:11:31+01:00",
+          "tree_id": "a3355dcce9d2df287ed756e2cd9472e648b2244e",
+          "url": "https://github.com/bitbacchus/spectre_benchmarks/commit/1ca7c888407c674405382dac90ef166fa5a35892"
+        },
+        "date": 1612861943812,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "test1234567890test1234567890",
+            "value": 176.049,
+            "range": "± 3.89403",
+            "unit": "ns",
+            "extra": "100 samples\n206 iterations"
+          }
+        ]
+      }
+    ]
+  }
+}
